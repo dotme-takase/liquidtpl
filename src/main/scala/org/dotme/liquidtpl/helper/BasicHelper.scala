0477034ed39e0543e03037bebd39e95aaac320c1
urashima-talk <urashima.talk@gmail.com>
2011-07-10 15:13:43 +0900

    json

--- src/main/scala/org/dotme/liquidtpl/helper/BasicHelper.scala
+++ src/main/scala/org/dotme/liquidtpl/helper/BasicHelper.scala
@@ -13,80 +13,98 @@ import javax.servlet.http.HttpServletResponse
 import org.dotme.liquidtpl.Constants
 import scala.collection.mutable.LinkedHashMap
 import scala.collection.mutable.ListBuffer
-import scala.xml.Atom
-import scala.xml.Node
+import scala.xml._
 import sjson.json.Format
 import sjson.json.JsonSerialization._
 import sjson.json.DefaultProtocol._
 
 object BasicHelper {
-  case class EscapedPCData(_data: String) extends Atom[String](_data)  {
+  case class EscapedPCData(_data: String) extends Atom[String](_data) {
     if (null == data) {
       throw new IllegalArgumentException("tried to construct EscapedPCData with null")
     }
 
     override def buildString(sb: StringBuilder) = {
       sb.append(Constants.LINE_SEPARATOR)
-      .append("//<![CDATA[")
-      .append(Constants.LINE_SEPARATOR)
-      .append("%s".format(data))
-      .append(Constants.LINE_SEPARATOR)
-      .append("//]]>")
-      .append(Constants.LINE_SEPARATOR)
+        .append("//<![CDATA[")
+        .append(Constants.LINE_SEPARATOR)
+        .append("%s".format(data))
+        .append(Constants.LINE_SEPARATOR)
+        .append("//]]>")
+        .append(Constants.LINE_SEPARATOR)
     }
   }
 
   def jsonFromIntStringPairs(list: List[(Int, String)]): JsValue =
-  {
-    val rs:List[JsValue] = list.map{ i =>
-      JsObject(List(
+    {
+      val rs: List[JsValue] = list.map { i =>
+        JsObject(List(
           (JsString(Constants.KEY_KEY) -> tojson(i._1)),
-          (JsString(Constants.KEY_VALUE) -> tojson(i._2))
-        ))
+          (JsString(Constants.KEY_VALUE) -> tojson(i._2))))
+      }
+      JsArray(rs)
     }
-    JsArray(rs)
-  }
 
   def jsonFromStringPairs(list: List[(String, String)]): JsValue =
-  {
-    val rs:List[JsValue] = list.map{ i =>
-      JsObject(List(
+    {
+      val rs: List[JsValue] = list.map { i =>
+        JsObject(List(
           (JsString(Constants.KEY_KEY) -> tojson(i._1)),
-          (JsString(Constants.KEY_VALUE) -> tojson(i._2))
-        ))
+          (JsString(Constants.KEY_VALUE) -> tojson(i._2))))
+      }
+      JsArray(rs)
     }
-    JsArray(rs)
-  }
 
-  def JsonTag(name:String, data:JsValue):Node = {
-    val buf:StringBuilder = new StringBuilder
+  def JsonTag(name: String, data: JsValue): Node = {
+    val buf: StringBuilder = new StringBuilder
     buf.append("var ")
-    .append(name)
-    .append(" = ")
-    .append(data.toString)
-    .append(";")
-    <script type="text/javascript">{EscapedPCData(buf.toString)}</script>
+      .append(name)
+      .append(" = ")
+      .append(jsValueOutPut(data))
+      .append(";")
+    <script type="text/javascript">{ EscapedPCData(buf.toString) }</script>
   }
 
-  def writeJsonCommentFiltered(response:HttpServletResponse, data:JsValue) = {
+  def writeJsonCommentFiltered(response: HttpServletResponse, data: JsValue) = {
     response.resetBuffer();
     response.setHeader(Constants.HEADER_CONTENT_TYPE, Constants.HEADER_CONTENT_TYPE_TEXT);
     response.setCharacterEncoding(Constants.CHARSET)
-    response.getWriter.print("/*%s*/".format(data.toString))
+    response.getWriter.print("/*%s*/".format(jsValueOutPut(data)))
     response.flushBuffer();
   }
 
-  def writeJsonRaw(response:HttpServletResponse, data:JsValue) = {
+  def writeJsonRaw(response: HttpServletResponse, data: JsValue) = {
     response.resetBuffer();
     response.setHeader(Constants.HEADER_CONTENT_TYPE, Constants.HEADER_CONTENT_TYPE_JSON);
     response.setCharacterEncoding(Constants.CHARSET)
-    response.getWriter.print("%s".format(data.toString))
+    response.getWriter.print("%s".format(jsValueOutPut(data)))
     response.flushBuffer();
   }
-  
+
+  def jsValueOutPut(data: JsValue): String = {
+    data.toString.replaceAll("/[\r]/g", "")
+      .replaceAll("/\r\n/g", "\n")
+      .replaceAll("/[\r\n]/g", "\\n").replace("/[\t]/g", "\\t")
+  }
+
   def attribute(kvs: (String, String)*) = new {
-    def +: (elem: xml.Elem) = elem % (xml.Node.NoAttributes /: kvs){
+    def +:(elem: xml.Elem) = elem % (xml.Node.NoAttributes /: kvs) {
       case (attr, (k, v)) => new xml.UnprefixedAttribute(k, v, attr)
+    }
+  }
+
+  def textToHtml(text: String): NodeSeq = {
+    text.replaceAll("\r\n", "\n").split("\r|\n|\r\n").flatMap { line =>
+      <p>{ autoLink(line) }</p>
+    }.toSeq
+  }
+
+  def autoLink(text: String): NodeSeq = {
+    val URLPATTERN = "((http|https|ftp):\\/\\/[\\w?=&.\\/-;#~%-]+(?![\\w\\s?&.\\/;#~%\"=-]*>))".r
+    URLPATTERN.findFirstMatchIn(text) match {
+      case None => Text(text)
+      case Some(m) =>
+        <xml:group>{ Text(m.before.toString) }<a target="_blank" class="ui-link" href={ m.matched.toString }>{ m.matched.toString }</a>{ autoLink(m.after.toString) }</xml:group>
     }
   }
 }
